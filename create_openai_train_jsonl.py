e4615364d7ddd5856e1dd3b9e7d0f2abf3d1c3e6
Or Groman <or.groman@nintex.com>
2025-01-12 11:15:52 +0200

    WIP

--- create_openai_train_jsonl.py
+++ create_openai_train_jsonl.py
@@ -1,9 +1,11 @@
+from functools import partial
 import json
 import logging
 from pathlib import Path
 from typing import List, Optional
 import pandas as pd
 from pydantic import BaseModel
+from tqdm import tqdm
 
 from runi_thesis_project.models.negation_detection.prompts import NEGATION_PROMPT_DETAILED
 
@@ -49,15 +51,16 @@ def jsonl_openai_line(row, column):
     return line
 
 def main():
-    no_ref_train_file = r''
-
+    tqdm.pandas()
     # Load the input csv, split to batches of 25,000 rows max each and prepare for openai
-    logger.info(f"Reading input file from: {args.input_tsv}")
-    file_type = Path(args.input_tsv).suffix
+    column = 'text_b'
+    input_file = Path(r'C:\workspace_or_private\repos\runi-thesis-project\hidrive\patentmatch_train_no_claims.csv')
+    logger.info(f"Reading input file from: {input_file}")    
+    file_type = input_file.suffix
     sep = "\t" if file_type == ".tsv" else ","
-    df = pd.read_csv(args.input_tsv, sep=sep)
-    column = args.column
-    output_dir = Path(args.input_tsv).parent / f"output_jsonl_{column}"
+    df = pd.read_csv(input_file, sep=sep)
+    column = 'text_b'
+    output_dir = input_file.parent / f"jsonl_{column}_{input_file.stem}"
     output_dir.mkdir(exist_ok=True)
     batch_size = 5000
     num_batches = len(df) // batch_size + 1
@@ -67,7 +70,7 @@ def main():
         end_idx = (i + 1) * batch_size
         batch_df = df.iloc[start_idx:end_idx]
         logger.info(f"Preparing batch {i + 1}/{num_batches} with {len(batch_df)} rows")
-        lines = batch_df.apply(jsonl_openai_line, axis=1)
+        lines = batch_df.progress_apply(jsonl_openai_line, axis=1, args=(column,))
 
         with open(output_dir / f"batch_{i}.jsonl", "w", encoding='utf-8') as f:
             for line in lines:
